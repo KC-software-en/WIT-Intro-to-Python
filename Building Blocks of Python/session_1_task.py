43d8973664d4b126aee3d94daa3cd9564707d184
Kisha Cairncross <142102107+KC-software-en@users.noreply.github.com>
2025-07-01 22:09:36 +0000

    Added session 1 task instructions.

---
+++ Building Blocks of Python/session_1_task.py
@@ -0,0 +1,7 @@
+"""Build a simple contact book or shopping list using lists and dictionaries.
+1. Write out the pseudocode for your chosen task.
+2. Document the description of the code with comments
+3. Name your objects with the snake case format.
+4. Use the various data types to store your data in the named variable.
+5. Use a list or dictionary to store your data.
+"""

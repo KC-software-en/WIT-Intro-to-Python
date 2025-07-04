062cf26248d19db3aa113580596cd1d1e1345a16
Kisha Cairncross <142102107+KC-software-en@users.noreply.github.com>
2025-07-04 18:09:40 +0000

    seperated zen from 1st program

---
+++ Building Blocks of Python/first_program.py
@@ -0,0 +1,8 @@
+# Lets initiate all newcomers to Python with the following right of passage: running your first Python program!
+# Uncomment the code below  
+# Run the program to view the output in the terminal
+# In the terminal, type: `python "first_program.py"` or click the play button in the top right corner
+
+# print("Hello, World!")
+
+
---
+++ Building Blocks of Python/zen_of_python.py
@@ -0,0 +1,7 @@
+# View the Zen of Python - a list of principles that influence the creation of the Python language 
+# NOTE: Uncomment the import 
+# Run the program
+# In the terminal, type: `python "zen_of_python.py"` or click the play button in the top right corner
+
+# import this
+

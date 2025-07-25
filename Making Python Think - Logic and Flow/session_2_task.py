451f2fc09dce0b0b578ee5a1c6fb1eac544efbef
Kisha Cairncross <142102107+KC-software-en@users.noreply.github.com>
2025-07-25 20:54:24 +0000

    added session 2 task

---
+++ Making Python Think - Logic and Flow/session_2_task.py
@@ -0,0 +1,12 @@
+"""Choose a program to build:
+
+- Even or Odd Checker: Enter a number, and the program tells you if it’s even or odd using % and conditionals.
+
+- Simple Chatbot: Respond to basic greetings or phrases using if/elif logic.
+
+1. Write out the pseudocode for your chosen task in a docstring.
+2. Document the description of the code with comments
+3. Name your objects with the snake case format.
+4. Use the various data types to store your data in the named variable.
+5. Use built-in functions at your discretion e.g. print() , input()
+"""

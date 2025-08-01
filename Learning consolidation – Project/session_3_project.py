5317dbddead1f5e949b82792d4b9263a5bb668d3
Kisha Cairncross <142102107+KC-software-en@users.noreply.github.com>
2025-08-01 21:22:29 +0000

    added session 3 project

---
+++ Learning consolidation – Project/session_3_project.py
@@ -0,0 +1,18 @@
+"""Password Strength Checker
+
+What it covers: Strings, conditionals, booleans, and loops
+Task requirements:
+1. Define a function called password_strength.
+2. Ask the user to input a password.
+3 Check for:
+3.1	Length (>= 8 characters)
+3.2	Presence of a number
+3.3	Presence of an uppercase letter
+4.	Give feedback like “Weak”, “Medium”, or “Strong”.
+
+Instructions:
+1. Write out the pseudocode for the task in a docstring.
+2. Document the description of the code with comments.
+3. Name your objects with the snake case format.
+4. Use built-in functions at your discretion (https://docs.python.org/3.11/library/stdtypes.html#built-in-types).
+"""
